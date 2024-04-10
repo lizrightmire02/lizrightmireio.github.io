9ceb6a5f5416193973d22fc84583b4963592e6a0
Liz Rightmire <129751290+lizrightmire02@users.noreply.github.com>
2024-04-10 10:13:17 -0400

    GradientDescentOptimizer

--- posts/LogisticRegression/logistic.py
+++ posts/LogisticRegression/logistic.py
@@ -91,8 +91,39 @@ class LogisticRegression(LinearModel):
         mean = torch.mean(grad, dim = 0)
         return mean
     
-    def step():
+class GradientDescentOptimizer:
+
+    def __init__(self, model):
+        self.model = model
+
+    def step(self, X, y, alpha, beta):
+        """
+        Compute a step of the gradient update
         
+        ARGUEMNTS:
+            X, torch.Tensor: the feature matrix. X.size() == (n, p), 
+            where n is the number of data points and p is the 
+            number of features. This implementation always assumes 
+            that the final column of X is a constant column of 1s. 
+            y, torch.Tensor: target vector, {0,1}. n x 1 dimensional.
+            alpha, float: learning rate parameter
+            beta, float: learning rate parameter. WHen beta = 0 we have 
+            regular gradient descent.
+        """
+        gradient = self.model.grad(X,y)
+        loss = self.model.loss(X,y)
+        currentWeight = self.model.w
+
+        # If first update, previous update does not exist so just alpha * gradient
+        if self.model.prev_w == None:
+            self.model.w -= alpha * gradient
+            
+        # all other updates
+        else:
+            self.model.w = currentWeight -1*alpha*gradient + beta*(currentWeight - self.model.prev_w)
+        
+        # update previous weight value
+        self.model.prev_w = currentWeight
 
 
 
